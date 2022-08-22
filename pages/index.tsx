0cd1978bb44d1c276e447db7764e605f28043998
barlette <leobarlette@gmail.com>
2022-08-21 22:39:59 -0300

    módulo finalizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Tarefa from '../model/Tarefa';
 import tarefasIniciais from '../data/mock';
 import Selecao from '../components/lista/Selecao';
 import ListaItem from '../components/lista/ListaItem';
+import Formulario from '../components/formulario/Formulario';
 import Lista from '../components/lista/Lista';
 import { useState } from 'react';
 import Cabecalho from '../template/Cabecalho';
@@ -9,13 +10,19 @@ import Conteudo from '../template/Conteudo';
 export default function Home() {
   const [tarefas, setTarefas] = useState(tarefasIniciais);
 
+  function novaTarefaCriada(novaTarefa: Tarefa) {
+    setTarefas(tarefas.adicionarTarefa(novaTarefa));
+  }
+
   return (
     <div
       className={`
       flex flex-col h-screen bg-gray-300
     `}
     >
-      <Cabecalho></Cabecalho>
+      <Cabecalho>
+        <Formulario novaTarefaCriada={novaTarefaCriada}></Formulario>
+      </Cabecalho>
       <Conteudo>
         <Lista
           tarefas={tarefas}
